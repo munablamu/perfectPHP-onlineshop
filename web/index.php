50f320159527bb533df089522d919464f0e4462a
Taro Harumaki <coffee.git@gmail.com>
2020-12-31 14:18:58 +0900

    To check php.ini for heroku

--- web/index.php
+++ web/index.php
@@ -3,5 +3,15 @@
 require '../bootstrap.php';
 require '../config/WebApplication.php';
 
-$app = new WebApplication(true);
+# PHP_TYPE is set in .htaccess
+switch ( getenv('PHP_TYPE') ) {
+  case 'module':
+    $debug = true;
+    break;
+  default:
+    $debug = false;
+    break;
+}
+phpinfo(); exit();
+$app = new WebApplication($debug);
 $app->run();
